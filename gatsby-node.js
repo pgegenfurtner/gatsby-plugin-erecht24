2b164915f9c1a03809a2ccbf1cc1bb00d3d51fa7
Philipp Gegenfurtner <philipp@pgegenfurtner.de>
2021-03-16 14:31:12 +0100

    fix async issue && bump version

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,11 @@
 const axios = require('axios');
 
+const asyncForEach = async (array, callback) => {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
 exports.sourceNodes = async ({ actions, createNodeId, createContentDigest, reporter }, { apiKey }) => {
 
   if (!apiKey) {
@@ -11,32 +17,37 @@ exports.sourceNodes = async ({ actions, createNodeId, createContentDigest, repor
   const endpoints = ['imprint', 'privacyPolicy']
 
   const fetchData = async (endpoint) => {
-    const data = await axios.get(apiUrl + endpoint, {
-      headers: {
-        eRecht24: apiKey
-      }
-    });
-    return data.data;
+    try {
+      const data = await axios.get(apiUrl + endpoint, {
+        headers: {
+          eRecht24: apiKey
+        }
+      });
+      return data.data;
+    } catch (e) {
+      reporter.warn('gatsby-plugin-erecht24: Could not fetch nodes', e);
+    }
   }
 
-  await endpoints.forEach(endpoint => {
+  const createLegalNode = async (data, endpoint) => {
+    const id = endpoint.charAt(0).toUpperCase() + endpoint.slice(1);
+    const node = {
+      ...data,
+      id: createNodeId(`E-Recht24-${id}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: `ERecht24${id}`,
+        contentDigest: createContentDigest(data)
+      }
+    }
+    await actions.createNode(node);
+  }
+
+  await asyncForEach(endpoints, async endpoint => {
     try {
-      fetchData(endpoint)
-        .then(data => {
-          const id = endpoint.charAt(0).toUpperCase() + endpoint.slice(1);
-          const node = {
-            ...data,
-            id: createNodeId(`E-Recht24-${id}`),
-            internal: {
-              type: `ERecht24${id}`,
-              contentDigest: createContentDigest(data)
-            }
-          }
-          actions.createNode(node);
-        })
-        .catch(e => {
-          reporter.warn('gatsby-plugin-erecht24: Could not fetch nodes', e);
-        })
+      const data = await fetchData(endpoint);
+      await createLegalNode(data, endpoint);
     } catch (e) {
       reporter.warn('gatsby-plugin-erecht24: Could not create nodes', e);
     }
